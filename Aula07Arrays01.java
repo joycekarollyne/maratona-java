42ec60cc331f78c6c564a643894802c24677c001
Joyce Karollyne <159137610+joycekarollyne@users.noreply.github.com>
2025-06-06 13:56:58 -0300

    Arrays

---
+++ Aula07Arrays01.java
@@ -0,0 +1,10 @@
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int [] idades = new int[3];
+        idades[0] = 15;
+        idades[1] = 20;
+        idades[2] = 30;
+        System.out.println(idades[0]);
+
+    }
+}
